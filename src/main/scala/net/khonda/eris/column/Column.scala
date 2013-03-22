d93c1c7d3349d3134f5a9b9f09aab41ea9a00f04
keisukehonda <keisukeh@gmail.com>
2013-03-23 00:27:25 +0900

    add lengh isEmpty method ColumnFamily

--- src/main/scala/net/khonda/eris/column/Column.scala
+++ src/main/scala/net/khonda/eris/column/Column.scala
@@ -10,8 +10,16 @@ object ConsistencyLevel extends Enumeration {
 case class Column(name: String, value: ByteString, timestamp: Long) extends NodeMessage
 
 case class ColumnFamily(key: Long, name: String, value: List[Column]) extends NodeMessage {
+
   def newBuilder: Unit = {
+
   }
+
+  def isEmpty: Boolean = value.isEmpty
+
+  def length: Int = value.length
+   
+
 }
 
 case class KeySpace(keyspace: String, value: List[ColumnFamily]) extends NodeMessage
